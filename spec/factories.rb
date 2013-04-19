60e0422c65775b5eaea4770a2a238eb5e815b83a
LeRoy Mattingly <leroy.mattingly@mac.com>
2013-04-19 06:02:43 -0500

    Finish updating users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do 
 	factory :user do			# automatically uses model class User
-		name "LeRoy Mattingly"
-		email "leroy@xxx.com"
+		sequence(:name)  { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" } 
+		# name "LeRoy Mattingly"
+		# email "leroy@xxx.com"
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
